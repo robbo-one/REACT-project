d62bf1b9e6ddf4256c13143ea4db8b9e163f78aa
Rob <robsc801@gmail.com>
2021-09-08 11:57:43 +1200

    use state to change title

--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateBasics = () => {
   // console.log(useState('hello world'))
   // const value = useState(1)[0]
   // const handler = useState(1)[1]
   // console.log(value, handler)
-  
-  return <h2>useState basic example</h2>;
-};
+  const [text, setText] = useState('random title') //you can name this anything for example;[poop, setPoop], and the initial state could be anything also ex. = useState('poop')
 
-export default UseStateBasics;
+  const handleClick = () => {
+    setText('hello world')
+  }
+  return <React.Fragment>
+    <h1>{text}</h1>
+    <button className="btn" onClick={handleClick}>
+      change title
+    </button>
+    </React.Fragment>
+}
+
+export default UseStateBasics
