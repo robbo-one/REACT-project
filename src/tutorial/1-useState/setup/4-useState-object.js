4f5386cf62b845898a2d610cca7faf9db287b398
Rob <robsc801@gmail.com>
2021-09-08 14:02:38 +1200

    use spread operator to change message of person

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,8 +7,21 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   })
-  console.log(person)
-  return <h2>useState object example</h2>
+  
+  const changeMessage = () => {
+    //below is using the spread operator to only change the message, not the name or age of the person.
+    setPerson({...person, message:'hello world' })
+  }
+  return (
+    <>
+      <h3>{person.name}</h3>
+      <h3>{person.age}</h3>
+      <h3>{person.message}</h3>
+      <button className='btn' onClick={changeMessage}>
+        change message
+      </button>
+    </>
+  )
 }
 
 export default UseStateObject
