77d5c5ea24fb58b0ab848b284203bc9e9457eae9
Rob <robsc801@gmail.com>
2021-09-08 13:57:23 +1200

    set up hook

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
-};
+  // setting up the hook
+  const [person, setPerson] = useState({
+    name: 'peter',
+    age: 24,
+    message: 'random message',
+  })
+  console.log(person)
+  return <h2>useState object example</h2>
+}
 
-export default UseStateObject;
+export default UseStateObject
