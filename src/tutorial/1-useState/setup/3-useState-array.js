55dc35ec300aec3f0a7311c5771b8e67eca16890
Rob <robsc801@gmail.com>
2021-09-08 13:52:29 +1200

    add remove Item variable and button to remove one person

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,6 +3,12 @@ import { data } from '../../../data'
 
 const UseStateArray = () => {
   const [people, setPeople] = React.useState(data)
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person)=> person.id !== id)
+setPeople(newPeople)
+    }
+  
   return (
     <>
       {people.map((person) => {
@@ -10,6 +16,8 @@ const UseStateArray = () => {
         return (
           <div key={id} className='item'>
             <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove
+            </button>
           </div>
         )
       })}
