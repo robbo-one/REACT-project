d23ccda6304d8609e1231d924f7888e7c139cb35
Rob <robsc801@gmail.com>
2021-09-08 13:46:24 +1200

    add a clear items button

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import { data } from '../../../data';
+import React from 'react'
+import { data } from '../../../data'
 
 const UseStateArray = () => {
-  const [people,setPeople] = React.useState(data)
-  return <>
-  {
-    people.map((person)=>{
-      const {id,name} = person
-      return <div key={id} className="item">
-        <h4>{name}</h4>
-      </div>
-    })
-  } 
-  </>
-};
+  const [people, setPeople] = React.useState(data)
+  return (
+    <>
+      {people.map((person) => {
+        const { id, name } = person
+        return (
+          <div key={id} className='item'>
+            <h4>{name}</h4>
+          </div>
+        )
+      })}
+      <button className="btn" onClick={() => setPeople([])}>
+        clear items
+      </button>
+    </>
+  )
+}
 
-export default UseStateArray;
+export default UseStateArray
