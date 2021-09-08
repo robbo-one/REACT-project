9eab45174f870ceacfc9a4aa1421467fdf67da66
Rob <robsc801@gmail.com>
2021-09-08 14:07:51 +1200

    set up state values for age, name and message

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -7,16 +7,21 @@ const UseStateObject = () => {
     age: 24,
     message: 'random message',
   })
+//below is setting up the state values
+  const [name, setName] = useState('peter')
+  const [age, setAge] = useState(24)
+  const [message, setMessage] = useState('random message')
   
   const changeMessage = () => {
     //below is using the spread operator to only change the message, not the name or age of the person.
-    setPerson({...person, message:'hello world' })
+    //setPerson({...person, message:'hello world' })
+    setMessage('hello world')
   }
   return (
     <>
-      <h3>{person.name}</h3>
-      <h3>{person.age}</h3>
-      <h3>{person.message}</h3>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h3>{message}</h3>
       <button className='btn' onClick={changeMessage}>
         change message
       </button>
